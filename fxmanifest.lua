c692a77f172cb314f8d778b692e85c1c34f28ba3
ttvmagma <93156341+ttvmagma@users.noreply.github.com>
2024-04-15 22:33:16 +0200

    fxmanifest

---
+++ fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'ttvmagma'
+description 'by xenon-service https://discord.gg/xenon-v2'
+
+client_scripts {
+    'client/client.lua'
+}
+
+server_scripts {
+    'server/server.lua'
+}
